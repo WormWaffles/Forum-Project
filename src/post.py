169ed56127c7d4571adc2b51bcce1809bf53c325
cbrow353 <cbrow353@uncc.edu>
2023-03-17 19:51:07 -0400

    Created post and post feed objects

---
+++ src/post.py
@@ -0,0 +1,12 @@
+from random import randint
+
+class Post:
+    def __init__(self, post_id, title, content, file, likes, dislikes, comments) -> None:
+        self.post_id = post_id
+        self.title = title
+        self.content = content
+        self.file = file
+        self.likes = likes
+        self.dislikes = dislikes
+        self.comments = comments
+        self.post = {'id': self.post_id, 'title': self.title, 'content': self.content, 'file': self.file, 'likes': self.likes, 'dislikes': self.dislikes, 'comments': self.comments}
