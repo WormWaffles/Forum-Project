a33afb29fa7023fa94ab303e5fcc455bbfc1e798
cbrow353 <cbrow353@uncc.edu>
2023-03-17 12:24:01 -0400

    Working on post ability

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+posts = {}
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
+
+# go to create post page
+@app.get('/create')
+def create():
+    return render_template('create.html')
+
+# create post
+@app.post('/add_post')
+def add_post():
+    title = request.form.get('title')
+    content = request.form.get('content')
+    file = request.files['file']
+    # ill do something with the file later **
+    posts[title] = {'content': content}
+    print(posts)
+    return redirect('/')
