727c8b8815d0133a23c8cc0218307e75e7f8657b
cbrow353 <cbrow353@uncc.edu>
2023-03-17 17:28:37 -0400

    Added post feature, along with ability to like and dislike.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, request
+import random
 
 app = Flask(__name__)
 
-posts = {}
+posts: dict[int, dict] = { 200 : {'title': 'Post Title', 'content': 'I really like my dog!', 'file': "", 'likes': 0, 'dislikes': 0, 'comments': {} } }
 
 @app.route('/')
 def index():
@@ -20,6 +21,22 @@ def add_post():
     content = request.form.get('content')
     file = request.files['file']
     # ill do something with the file later **
-    posts[title] = {'content': content}
+    # get random id
+    post_id = random.randint(1, 100000)
+    posts[post_id] = {'title': title, 'content': content, 'file': "", 'likes': 0, 'dislikes': 0, 'comments': {}}
     print(posts)
+    return redirect('/')
+
+# like post
+@app.post('/like_post')
+def like_post():
+    post_id = int(request.form.get('post_id'))
+    posts[post_id]['likes'] += 1
+    return redirect('/')
+
+# dislike post
+@app.post('/dislike_post')
+def dislike_post():
+    post_id = int(request.form.get('post_id'))
+    posts[post_id]['dislikes'] += 1
     return redirect('/')
