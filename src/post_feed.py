1bd7e38bcdd380d1a2c5cae8de9fc25be017768c
cbrow353 <cbrow353@uncc.edu>
2023-04-26 12:37:49 -0400

    fixed location bugs

--- src/post_feed.py
+++ src/post_feed.py
@@ -54,8 +54,7 @@ class PostFeed:
         """))
         post_objects = []
         for post in posts:
-            print(post)
-            post_object = Post(post_id=post[0], user_id=post[1], title=post[2], content=post[3], file=post[4], post_date=post[5], likes=post[6], event=post[7], from_date=post[8], to_date=post[9], check_in=post[10], location=post[11], user=User(user_id=post[12], username=post[13], password=post[14], first_name=post[15], last_name=post[16], email=post[17], about_me=post[18], location=post[19], private=post[20], profile_pic=post[21], banner_pic=post[22], is_business=post[23], address=post[24], city=post[25], state=post[26], zip_code=post[27], phone=post[28], website=post[29]))
+            post_object = Post(post_id=post[0], user_id=post[1], title=post[2], content=post[3], file=post[4], post_date=post[5], likes=post[6], event=post[7], from_date=post[8], to_date=post[9], location=post[10], comments=post[11], check_in=post[12], user=User(user_id=post[13], username=post[14], password=post[15], first_name=post[16], last_name=post[17], email=post[18], about_me=post[19], location=post[20], private=post[21], profile_pic=post[22], banner_pic=post[23], is_business=post[24], address=post[25], city=post[26], state=post[27], zip_code=post[28], phone=post[29], website=post[30]))
             post_objects.append(post_object)
         return post_objects
 
