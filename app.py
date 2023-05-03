16f4ec6ab4ca4c68aa78b298f431607760309900
Jai_Vang <jaivang0387@gmail.com>
2023-05-03 19:58:01 -0400

    fixing comments

--- app.py
+++ app.py
@@ -848,16 +848,13 @@ def googlelogin():
 # delete user
 @app.route('/delete/<int:id>')
 def delete(id):
-    #get record of user based on id and set to variable
-    # user_to_delete = users.query.get_or_404(id)
+    #delete user based on id and assign to variable
     user_to_delete = users.delete_user(id)
-
     #delete user from database
     db.session.delete(user_to_delete)
     #commit delete
     db.session.commit()
     #afterward, return to home page
-
     return redirect('/')
     
 
