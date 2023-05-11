d1505446bdb80a72187c479373b543eb9c8ed5a0
FredSchuck <fredschuck@gmail.com>
2023-05-11 13:31:28 -0400

    Added additional db clears

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,10 +1,16 @@
 import pytest
 from app import app
-from src.models import *  #import all db objects? i.e. Users, Rating, Post_Feed, etc.?
+from src.models import *
 
 @pytest.fixture(scope='module')
 def test_client():
     with app.app_context():
         User.query.delete()
+        Post.query.delete()
+        Rating.query.delete()
+        Follower.query.delete()
+        Comment.query.delete()
+        BusinessItems.query.delete()
+        UserLikes.query.delete()
         db.session.commit()
         yield app.test_client()
